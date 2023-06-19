53408af33930ce69f02fbff20454fd466e48716a
yaroslav.horban <yaroslav.horban@codeit.pro>
2023-06-19 14:54:45 +0300

    Port fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { config } from 'dotenv';
 config({ path: '../src' });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('app running ', process.env.PORT);
+  console.log('app running ', process.env.PORT || 80);
   await app.listen(process.env.PORT);
 }
 
