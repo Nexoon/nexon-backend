7691afdb20ffc8572682db12492f39dfa64bee17
yaroslav.horban <yaroslav.horban@codeit.pro>
2023-06-19 13:22:27 +0300

    Environment variables config added

--- dist/main.js
+++ dist/main.js
@@ -2,8 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)({ path: '../src' });
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    console.log('app running ', process.env.PORT);
     await app.listen(process.env.PORT);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 
-config();
+config({ path: '../src' });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('app running ', process.env.PORT);
   await app.listen(process.env.PORT);
 }
 
