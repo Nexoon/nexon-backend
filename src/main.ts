ec153bb860b2d3a3e55de1abd17c8a380a8fded4
yaroslav.horban <yaroslav.horban@codeit.pro>
2023-06-19 15:00:21 +0300

    Port fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ config({ path: '../src' });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('app running ', process.env.PORT || 80);
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 80);
 }
 
 bootstrap();
